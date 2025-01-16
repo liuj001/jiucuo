6ef9fc471535058042c58e4e89a01c664d1b6cc4
bbbj001 <1365940206@qq.com>
2025-01-16 12:10:31 +0800

    runJiuCuo.sh

--- runJiuCuo.sh
+++ runJiuCuo.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 identity_value=0.7
-num_threads=8
-eps=100
-min_samples=3
+threads=8
+diameter_size=600
+cluster_size=3
 k_size=8
 min_bases=1
 min_reads=3
@@ -10,6 +10,27 @@ allocated_reads=10000
 bam="/raw.bam"
 bam_f="/filt.bam"
 adaptor_removal=0
+
+# 参数校验函数
+validate_param() {
+  local value=$1
+  local min=$2
+  local max=$3
+  local name=$4
+  local is_integer=$5
+
+  if [ "$is_integer" == "1" ]; then
+    if ! [[ "$value" =~ ^[0-9]+$ ]]; then
+      echo "Error: $name must be an integer."
+      exit 1
+    fi
+  fi
+
+  if (( $(echo "$value < $min" | bc -l) || $(echo "$value > $max" | bc -l) )); then
+    echo "Error: $name must be between $min and $max."
+    exit 1
+  fi
+}
 
 # 处理命令行参数
 while [[ $# -gt 0 ]]; do
@@ -35,14 +56,14 @@ while [[ $# -gt 0 ]]; do
     -identity_value)     
       identity_value=$2
       shift 2 ;;
-    -eps)      
-      eps=$2
+    -diameter_size)      
+      diameter_size=$2
       shift 2 ;;
-    -num_threads)      
-      num_threads=$2
+    -threads)      
+      threads=$2
       shift 2 ;;
-    -min_samples)     
-      min_samples=$2
+    -cluster_size)     
+      cluster_size=$2
       shift 2 ;;
     -k_size)      
       k_size=$2
@@ -56,6 +77,16 @@ while [[ $# -gt 0 ]]; do
   esac
 done
 
+# 校验参数
+validate_param "$identity_value" 0 1 "identity_value" 0
+validate_param "$diameter_size" 0 5000 "diameter_size" 0
+validate_param "$cluster_size" 2 10 "cluster_size" 1
+validate_param "$threads" 1 100 "threads" 1
+validate_param "$k_size" 1 30 "k_size" 1
+validate_param "$min_bases" 1 5 "min_bases" 1
+validate_param "$min_reads" 1 100 "min_reads" 1
+validate_param "$allocated_reads" 100 1000000 "allocated_reads" 1
+
 # 创建输出目录（如果不存在）
 if [ ! -d "$output" ]; then
   mkdir "$output"
@@ -67,7 +98,7 @@ if [ ! -f "$output$bam" ]; then
 fi
 
 # 运行 JiuCuo 脚本
-python runJiuCuo.py -contigs "$contigs" -reads "$reads" -output "$output" -min_bases "$min_bases" -min_reads "$min_reads" -allocated_reads "$allocated_reads" -adaptor_removal "$adaptor_removal"
+python runJiuCuo.py -contigs "$contigs" -reads "$reads" -output "$output" -threads  "$threads" -min_bases "$min_bases" -min_reads "$min_reads" -allocated_reads "$allocated_reads" -adaptor_removal "$adaptor_removal"
 
 # 初始化变量
 num=1
@@ -89,9 +120,9 @@ if [ "$adaptor_removal" -eq "$num" ]; then
                --bamview_file "$output$bamview_file" \
                --process_files "$output$process_files" \
                --similarity "$identity_value" \
-               --num_threads "$num_threads" \
-               --eps "$eps" \
-               --min_samples "$min_samples" \
+               --num_threads "$threads" \
+               --eps "$diameter_size" \
+               --min_samples "$cluster_size" \
                --k_size "$k_size" \
                --images_dir "$output$adapter" \
                --output_images_dir "$output$adapter_out" \
