7d066a2aec8c91373c055255a7db19250c7b4cc6
bbbj001 <1365940206@qq.com>
2025-01-20 01:17:21 +0800

    yolo/process/run.sh

--- yolo/process/run.sh
+++ yolo/process/run.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-python run.py  --bam_filename "/root/autodl-tmp/adapter/H_h/H_m_no_qualities_new.bam" \
-               --bamview_file "/root/autodl-tmp/adapter/H_h/bamview-new.csv"\
-               --process_files "/root/autodl-tmp/pmf/pmf_dev2/data/detection_results.csv" \
+python run.py  --bam_filename "/root/autodl-tmp/HG002/adapter/chr21_adapter.bam" \
+               --bamview_file "/root/autodl-tmp/HG002/adapter/bamview-new.csv"\
+               --images_dir "/root/autodl-tmp/HG002/adapter/images_0106" \
                --similarity 0.7 \
                --num_threads 1 \
                --eps 100 \
-               --min_samples 2 \
+               --min_samples 2\
                --k_size 8\
-               --images_dir "/root/autodl-tmp/adapter/H_h/images_m" \
-               --output_images_dir "/root/autodl-tmp/pmf/pmf_dev2/output_images" \
-               --output_detection_csv_path "/root/autodl-tmp/pmf/pmf_dev2/data/detection_results.csv" \
-               --adapter_output_dir "/root/autodl-tmp/pmf/pmf_dev2/result"\
-               --is_inference 1
+               --adapter_output_dir "../result_chr_0118_images"\
+
+
+
+
