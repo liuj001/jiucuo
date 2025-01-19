937f9b74faff71363671431ef1f8b9c09c759e20
bbbj001 <1365940206@qq.com>
2025-01-20 01:18:47 +0800

    yolo/process/run_with_default.sh

---
+++ yolo/process/run_with_default.sh
@@ -0,0 +1,3 @@
+python run.py  --bam_filename "/root/autodl-tmp/HG002/adapter/chr21_adapter.bam" \
+               --bamview_file "/root/autodl-tmp/HG002/adapter/bamview-new.csv"\
+               --images_dir "/root/autodl-tmp/HG002/adapter/images_0106" \
