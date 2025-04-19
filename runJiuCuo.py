8e9aea540d27f85f5c825961fbb25fc89d20b983
liuj001 <1365940206@qq.com>
2025-04-19 23:06:15 +0800

    runJiuCuo.py_jiucuo_v2

--- runJiuCuo.py
+++ runJiuCuo.py
@@ -3,11 +3,14 @@ import os
 import argparse
 import pysam
 from time import *
-from concurrent.futures import ThreadPoolExecutor
+#from concurrent.futures import ThreadPoolExecutor
+from concurrent.futures import ProcessPoolExecutor, as_completed
 from threading import Thread
 import subprocess
 import sys
 import logging
+import torch
+import multiprocessing
 
 from tqdm import tqdm
 from picture.candidate_make import candidate_make
@@ -16,10 +19,12 @@ from picture.bamview_new import bamview
 from picture.add_pos import add_pos
 from picture.Ex_k4_adapter_ver2 import adapter_pic
 from inception.predict_a import find_snp
+from inception.utils_a import get_network
 from correction.find_error import find_error
 from correction.read_locate_indel_v5 import correct
 
 import glob
+#os.environ["CUDA_VISIBLE_DEVICES"]="0"
 
 parser=argparse.ArgumentParser(description='runJiuCuo.py integrates the processes of error correction.')
 parser.add_argument('-contigs',type=str,help='Preassembled primary contigs from the reads in FASTA format',required=True)
@@ -29,7 +34,8 @@ parser.add_argument('-output',type=str,help='Output directory',required=True)
 #parser.add_argument('-min_reads',type=int,help='Minimum number of reads required to generate the error candidate image',default=3)
 parser.add_argument('-threads',type=int,help='Number of threads during correction',default=8)
 parser.add_argument('-allocated_reads',type=int,help='Maximum number of reads allocated to each thread',default=10000)
-parser.add_argument('-adapter_removal',type=int,help='Adapter removal from the reads.',default=0)
+# parser.add_argument('-adapter_removal',type=int,help='Adapter removal from the reads.',default=0)
+parser.add_argument('-error_correction',type=int,help='correct error from the reads.',default=1)
 args=parser.parse_args()
 
 ref = args.contigs
@@ -39,11 +45,13 @@ outdir = args.output
 #min_reads = args.min_reads
 thread = args.threads
 allocated_reads = args.allocated_reads
-adapter_removal = args.adapter_removal
+# adapter_removal = args.adapter_removal
+error_correction = args.error_correction
+
 bam = outdir+'/raw.bam'
 bam_dir = outdir+'/bam'
 txt_dir = outdir+'/txt'
-txt_add_dir = outdir+'/txt_add'
+# txt_add_dir = outdir+'/txt_add'
 bcf_txt_dir = outdir+'/bcf_txt'
 snp_c_dir = outdir+'/snp_candidate'
 adapter_dir = outdir+'/adapter'
@@ -63,8 +71,8 @@ if not os.path.exists(bam_dir):
     os.makedirs(bam_dir)
 if not os.path.exists(txt_dir):
     os.makedirs(txt_dir)
-if not os.path.exists(txt_add_dir):
-    os.makedirs(txt_add_dir)
+# if not os.path.exists(txt_add_dir):
+#     os.makedirs(txt_add_dir)
 if not os.path.exists(bcf_txt_dir):
     os.makedirs(bcf_txt_dir)
 if not os.path.exists(snp_c_dir):
@@ -102,6 +110,13 @@ def re_refname(chr):
 
 def error_correct(chr):
     start = time()
+    # 预加载模型
+    multiprocessing.set_start_method('spawn', force=True)
+    net = get_network()
+    #net.load_state_dict(torch.load("inception/weight/inceptionv4.pth"))
+    net.load_state_dict(torch.load("inception/weight/inceptionv4.pth", map_location='cpu'))
+    #net.cuda().eval()
+    net.to('cpu').eval()
     chr = chr.rstrip('.bam')
     reads_dir_path = c_reads_dir+'/'+chr+'.ec.fastq.gz'
     if not os.path.exists(reads_dir_path):
@@ -112,8 +127,8 @@ def error_correct(chr):
                 bai_code=bai_p.wait()  #等待子进程结束，并返回状态码;
             #samtools index -@ 48 -b /root/autodl-tmp/HG002/old/HG002.raw.sort.filt.bam
             #os.system("samtools index {out}/{chr}.bam {out}/{chr}.bam.bai".format(out=bam_dir,chr=chr))
-        if not os.path.exists(txt_dir+'/'+chr+'.txt'):
-            txt_cmd = "samtools mpileup -d 100000 -Q 0 -f %s %s/%s.bam > %s/%s.txt"%(ref, bam_dir, chr, txt_dir, chr)
+        if not os.path.exists(txt_dir+'/'+chr+'.txt') and error_correction == 1:
+            txt_cmd = "samtools mpileup -d 10000 -Q 0 -f %s %s/%s.bam > %s/%s.txt"%(ref, bam_dir, chr, txt_dir, chr)
             with open(log_file, "a") as log:
                 txt_p=subprocess.Popen(txt_cmd,shell=True, stdout=log, stderr=log)
                 txt_code=txt_p.wait()  #等待子进程结束，并返回状态码;
@@ -121,44 +136,41 @@ def error_correct(chr):
         snp_pic_dir = snp_c_dir+'/'+chr
         txt_file = txt_dir + '/' + chr + '.txt'
         sz = os.path.getsize(txt_file)
-        if not sz:
+
+        #判断文件是否完整
+        with open(txt_file, "rb") as f:
+            f.seek(-1, 2)  # 定位到文件末尾前1字节
+            last_char = f.read(1)
+            # print("有换行符" if last_char == b'\n' else "无换行符")
+        if not sz or last_char != b'\n':
+            print(chr + "had be killed")
             end = time()
             #print(chr,"done!", f'time: {end - start:.3f}s.=========================')
         else:  
-            if adapter_removal==1:
-                bamview_txt_cmd = "samtools view %s/%s.bam > %s/%s_bamview.txt"%(bam_dir, chr, bamview_txt_dir, chr)
-                with open(log_file, "a") as log:
-                    bamview_txt_p=subprocess.Popen(bamview_txt_cmd,shell=True, stdout=log, stderr=log)
-                    bamview_txt_code=bamview_txt_p.wait()  #等待子进程结束，并返回状态码;
-                csv_f=bamview_csv_dir+'/'+chr+'.csv'
-                txt_f=chr+'_bamview.txt'
-                bamview(csv_f,txt_f,bamview_txt_dir)
-                add_pos(chr, txt_dir, txt_add_dir)
-                adapter_pic(bam_dir, txt_add_dir, adapter_dir, chr)
-
-            #print(chr,"make candidate =========================")  
-            candidate_make(chr, txt_dir, bcf_txt_dir)
-            bcf_file = bcf_txt_dir + '/' + chr + '.bcf.txt'
-            szbcf = os.path.getsize(bcf_file)
-            if not szbcf:
-                #print(chr,"has no snp candidate")
-                nosnp=1
-            else:
-                #print(chr,"pic =========================")
-                if not os.path.exists(snp_pic_dir):
-                    os.makedirs(snp_pic_dir)
-                snp_pic(bam_dir, txt_dir, bcf_txt_dir, snp_pic_dir, chr)
-                #print(chr,"call snp =========================")
-                find_snp(snp_pic_dir, snp_dir, chr)
-            
-            #print(chr,"find error =========================")
-            find_error(ec_dir, snp_dir, txt_dir, chr)
-            #print(chr,"correct error =========================")
-            correct(bam_dir, txt_dir, ec_dir, c_reads_dir, chr)
-            end = time()
-            re_refname(chr)
+            if error_correction == 1:
+                # print(chr,"make candidate =========================")  
+                candidate_make(chr, txt_dir, bcf_txt_dir)
+                bcf_file = bcf_txt_dir + '/' + chr + '.bcf.txt'
+                szbcf = os.path.getsize(bcf_file)
+                if not szbcf:
+                    #print(chr,"has no snp candidate")
+                    nosnp=1
+                else:
+                    #print(chr,"pic =========================")
+                    if not os.path.exists(snp_pic_dir):
+                        os.makedirs(snp_pic_dir)
+                    snp_pic(bam_dir, txt_dir, bcf_txt_dir, snp_pic_dir, chr)
+                    #print(chr,"call snp =========================")
+                    find_snp(snp_pic_dir, snp_dir, chr, net)
+                
+                #print(chr,"find error =========================")
+                find_error(ec_dir, snp_dir, txt_dir, chr)
+                #print(chr,"correct error =========================")
+                correct(bam_dir, txt_dir, ec_dir, c_reads_dir, chr)
+                end = time()
+                re_refname(chr)
             #print(chr,"done!", f'time: {end - start:.3f}s.=========================')
-    
+    #torch.cuda.empty_cache()
     return chr
 
 def spilt(chr):
@@ -244,7 +256,7 @@ def spilt(chr):
 #     split_cmd = "samtools view -H %s | cut -f 2 | grep SN | cut -f 2 -d \":\" > %s/chr.txt"%(bam,txt_dir)
 #     os.system(split_cmd) #得到该bam文件的所有染色体号
 
-def main():
+def bam_file():
     if not os.path.exists(ref + '.fai'):
         faidx_cmd = f"samtools faidx {ref}"
         with open(log_file, "a") as log:
@@ -281,8 +293,11 @@ def file_filter(f):
 
 if __name__ == '__main__':
     start = time()
-    main()
+    bam_file()
     end_bam = time()
+
+    
+    
     #print(f'==============================Filt bam: {end_bam - start:.3f}s.==============================')
     
     #start_spilt = time()
@@ -298,106 +313,120 @@ if __name__ == '__main__':
         chr_n = chr_file.readlines()
 
     # 创建进度条
-    with tqdm(total=len(chr_n), desc="STAGE 2: SNP-aware error correction task allocation", bar_format="{l_bar}{bar} |") as pbar:
-        with ThreadPoolExecutor(max_workers=thread) as executor:
-            # 使用回调函数更新进度条
+    local_time = strftime("%Y-%m-%d %H:%M:%S", localtime())
+    print(local_time)
+    # with tqdm(total=len(chr_n), desc="STAGE 2: SNP-aware error correction task allocation", bar_format="{l_bar}{bar} |") as pbar:
+    #     with ProcessPoolExecutor(max_workers=thread) as executor:  # Changed to ProcessPoolExecutor
+    #         futures = [executor.submit(spilt, chr) for chr in chr_n]
+    #         for future in futures:
+    #             future.result()
+    #             pbar.update(1)
+    if error_correction == 1:
+        with tqdm(total=len(chr_n), desc="STAGE 2: Read preprocessing", bar_format="{l_bar}{bar} |") as pbar:
+            with ProcessPoolExecutor(max_workers=thread) as executor:
+                futures = [executor.submit(spilt, chr) for chr in chr_n]
+                # 按任务完成顺序处理
+                for future in as_completed(futures):  # 动态监听完成事件
+                    result = future.result()         # 获取已完成任务的结果
+                    pbar.update(1) 
+    else:
+        with ProcessPoolExecutor(max_workers=thread) as executor:
             futures = [executor.submit(spilt, chr) for chr in chr_n]
-            for future in futures:
-                future.result()
-                pbar.update(1)
+            # 按任务完成顺序处理
+            for future in as_completed(futures):  # 动态监听完成事件
+                result = future.result()         # 获取已完成任务的结果
+                pbar.update(1)   
 
     end_spilt = time()
+    
 
-    # start_error = time()
-    # chr_list= os.listdir(bam_dir)
-    # chr_l = list(filter(file_filter, chr_list))
-    # with ThreadPoolExecutor(max_workers=thread) as executor:
-    #     res = executor.map(error_correct, chr_l)
-    # end_error = time()
-    #print(f'==============================Correct error: {end_error - start_error:.3f}s.==============================')    
     start_error = time()
+    
+
     chr_list = os.listdir(bam_dir)
     chr_l = list(filter(file_filter, chr_list))
 
     # 创建进度条
-    with tqdm(total=len(chr_l), desc="STAGE 3: SNP-aware error correction", bar_format="{l_bar}{bar} |") as pbar:
-        with ThreadPoolExecutor(max_workers=thread) as executor:
-            futures = [executor.submit(error_correct, chr) for chr in chr_l]
-            for future in futures:
-                future.result()
-                pbar.update(1)
+    local_time = strftime("%Y-%m-%d %H:%M:%S", localtime())
+    print(local_time)
+    # with tqdm(total=len(chr_l), desc="STAGE 3: SNP-aware error correction", bar_format="{l_bar}{bar} |") as pbar:
+    #     with ProcessPoolExecutor(max_workers=thread) as executor:  # Changed to ProcessPoolExecutor
+    #         futures = [executor.submit(error_correct, chr) for chr in chr_l]
+    #         for future in futures:
+    #             future.result()
+    #             pbar.update(1)
+    if error_correction == 1:
+        with tqdm(total=len(chr_l), desc="STAGE 3: SNP-aware error detection", bar_format="{l_bar}{bar} |") as pbar:
+            with ProcessPoolExecutor(max_workers=thread) as executor:
+                futures = [executor.submit(error_correct, chr) for chr in chr_l]
+                # 按任务完成顺序处理
+                for future in as_completed(futures):  # 动态监听完成事件
+                    result = future.result()         # 获取已完成任务的结果
+                    pbar.update(1)                  # 立即更新进度条              # 立即更新进度条
+    else:
+        with tqdm(total=len(chr_l), desc="STAGE 2: Read preprocessing", bar_format="{l_bar}{bar} |") as pbar:
+            with ProcessPoolExecutor(max_workers=thread) as executor:
+                futures = [executor.submit(error_correct, chr) for chr in chr_l]
+                # 按任务完成顺序处理
+                for future in as_completed(futures):  # 动态监听完成事件
+                    result = future.result()         # 获取已完成任务的结果
+                    pbar.update(1)                  # 立即更新进度条              # 立即更新进度条
+
 
     end_error = time()
-     
-    start_read = time()
-    # 显示拼接进度
-    with tqdm(total=3, desc="STAGE 4: SNP-aware error correction results writing to file", bar_format="{l_bar}{bar} |") as pbar:
-        read_cmd = "cat %s/*.fastq > %s/reads_c.fastq" % (c_reads_dir, outdir)
+    
+    # start_error = time()
+    # chr_list = os.listdir(bam_dir)
+    # chr_l = list(filter(file_filter, chr_list))
+
+    # # 创建进度条
+    # with tqdm(total=len(chr_l), desc="STAGE 3: SNP-aware error correction", bar_format="{l_bar}{bar} |") as pbar:
+    #     with ThreadPoolExecutor(max_workers=thread) as executor:
+    #         futures = [executor.submit(error_correct, chr) for chr in chr_l]
+    #         for future in futures:
+    #             future.result()
+    #             pbar.update(1)
+
+    # end_error = time()
+    if error_correction == 1:
+        start_read = time()
+        # 显示拼接进度
+        local_time = strftime("%Y-%m-%d %H:%M:%S", localtime())
+        print(local_time)
+        with tqdm(total=3, desc="STAGE 4: SNP-aware error correction", bar_format="{l_bar}{bar} |") as pbar:
+            read_cmd = "cat %s/*.fastq > %s/reads_c.fastq" % (c_reads_dir, outdir)
+            with open(log_file, "a") as log:
+                read_p = subprocess.Popen(read_cmd, shell=True, stdout=log, stderr=log)
+                read_code = read_p.wait()
+            pbar.update(1)
+    
+            com_cmd = "seqkit common %s %s/reads_c.fastq | seqkit seq -n -i -o %s/common.txt" % (reads_path, outdir, outdir)
+            with open(log_file, "a") as log:
+                com_p = subprocess.Popen(com_cmd, shell=True, stdout=log, stderr=log)
+                com_code = com_p.wait()
+            pbar.update(1)
+    
+            rem_cmd = "seqkit grep -v -f %s/common.txt %s -o %s/s.fastq" % (outdir, reads_path, outdir)
+            with open(log_file, "a") as log:
+                rem_p = subprocess.Popen(rem_cmd, shell=True, stdout=log, stderr=log)
+                rem_code = rem_p.wait()
+            pbar.update(1)
+    
+        t = outdir + "/common.txt"
+        os.remove(t)
+    
+        end_cmd = "cat %s/s.fastq %s/reads_c.fastq > %s/base_correction.fastq" % (outdir, outdir, outdir)
         with open(log_file, "a") as log:
-            read_p = subprocess.Popen(read_cmd, shell=True, stdout=log, stderr=log)
-            read_code = read_p.wait()
-        pbar.update(1)
-
-        com_cmd = "seqkit common %s %s/reads_c.fastq | seqkit seq -n -i -o %s/common.txt" % (reads_path, outdir, outdir)
-        with open(log_file, "a") as log:
-            com_p = subprocess.Popen(com_cmd, shell=True, stdout=log, stderr=log)
-            com_code = com_p.wait()
-        pbar.update(1)
-
-        rem_cmd = "seqkit grep -v -f %s/common.txt %s -o %s/s.fastq" % (outdir, reads_path, outdir)
-        with open(log_file, "a") as log:
-            rem_p = subprocess.Popen(rem_cmd, shell=True, stdout=log, stderr=log)
-            rem_code = rem_p.wait()
-        pbar.update(1)
-
-    t = outdir + "/common.txt"
-    os.remove(t)
-
-    end_cmd = "cat %s/s.fastq %s/reads_c.fastq > %s/base_correction.fastq" % (outdir, outdir, outdir)
-    with open(log_file, "a") as log:
-        end_p = subprocess.Popen(end_cmd, shell=True, stdout=log, stderr=log)
-        end_code = end_p.wait()
-
-    c = outdir + "/reads_c.fastq"
-    os.remove(c)
-    s = outdir + "/s.fastq"
-    os.remove(s)
-
-    end = time()
-
-
-    # start_read = time()
-    # #print("Get reads ====================")
-    # read_cmd = "cat %s/*.gz > %s/reads_c.fastq.gz"%(c_reads_dir, outdir) 
-    # read_p=subprocess.Popen(read_cmd,shell=True)
-    # read_code=read_p.wait()
-
-    # #print("Find common====================")
-    # com_cmd = "seqkit common %s %s/reads_c.fastq.gz | seqkit seq -n -i -o %s/common.txt"%(reads_path, outdir, outdir)
-    # #seqkit common file*.fa | seqkit seq -n -i -o common.txt
-    # com_p=subprocess.Popen(com_cmd,shell=True)
-    # com_code=com_p.wait()
-
-    # #print("Remove====================")
-    # rem_cmd = "seqkit grep -v -f %s/common.txt %s -o %s/s.fastq.gz"%(outdir, reads_path, outdir)
-    # #seqkit grep -v -f common.txt a.fa -o common.txt
-    # rem_p=subprocess.Popen(rem_cmd,shell=True)
-    # rem_code=rem_p.wait()
-
-    # t = outdir+"/common.txt"
-    # os.remove(t)
-
-    # #print("Cat====================")
-    # end_cmd = "cat %s/s.fastq.gz %s/reads_c.fastq.gz > %s/correction.fastq.gz"%(outdir, outdir, outdir)
-    # #cat Sample_test_1.R1.fastq.gz Sample_test_2.R2.fastq.gz > test2.fastq.gz
-    # end_p=subprocess.Popen(end_cmd,shell=True)
-    # end_code=end_p.wait()
-
-    # c = outdir+"/reads_c.fastq.gz"
-    # os.remove(c)
-    # s = outdir+"/s.fastq.gz"
-    # os.remove(s)
-
-    # end = time()
-    # #print(f'==============================Total time: {end - start_read:.3f}s.==============================')
-    # #print(f'==============================Total time: {end - start:.3f}s.==============================')
+            end_p = subprocess.Popen(end_cmd, shell=True, stdout=log, stderr=log)
+            end_code = end_p.wait()
+    
+        c = outdir + "/reads_c.fastq"
+        os.remove(c)
+        s = outdir + "/s.fastq"
+        os.remove(s)
+        end_read = time()
+        
+        end = time()
+    
+    
+    
