9ed45a5ec1b71c403cca61a6f37eeb111b1341b2
liuj001 <1365940206@qq.com>
2025-04-20 22:32:42 +0800

    yolo/finetune/train.py_jiucuo_v2

--- yolo/finetune/train.py
+++ yolo/finetune/train.py
@@ -1,4 +1,14 @@
+import argparse
 from ultralytics import YOLO
+
+# 解析命令行参数
+parser = argparse.ArgumentParser(description='Train YOLOv8 model with custom parameters.')
+parser.add_argument('-lr', type=float, default=0.001, help='Initial learning rate')
+parser.add_argument('-b', type=int, default=1, help='Batch size for training')
+parser.add_argument('-epochs', type=int, default=400, help='Number of epochs to train')
+parser.add_argument('-imgsz', type=int, default=1471, help='Image size for training')
+parser.add_argument('-data', type=str, default="data.yaml", help='Path to data configuration file')
+args = parser.parse_args()
 
 # Load a COCO-pretrained YOLOv8n model
 model = YOLO("yolov8n.pt")
@@ -6,6 +16,11 @@ model = YOLO("yolov8n.pt")
 # Display model information (optional)
 model.info()
 
-# Train the model on the COCO8 example dataset for 100 epochs
-results = model.train(data="data.yaml", epochs=400, imgsz=1471)
-
+# Train the model with parameters from command line
+results = model.train(
+    data=args.data,
+    epochs=args.epochs,
+    imgsz=args.imgsz,
+    lr0=args.lr,
+    batch=args.b
+)
